c74f202dcd0f8b49625249cc587f8c6d51059dcb
theHarvester <james.harvey994@gmail.com>
2015-09-07 21:39:12 +1000

    Added images

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,7 @@ gulp.task('less', function() {
   gulp.src('less/*.less')
     .pipe(less())
     .pipe(gulp.dest('css'));
-    // .pipe(livereload());
 });
-
-// gulp.task('watch', function() {
-//   livereload.listen();
-//   // gulp.watch('less/*.less', ['less']);
-// });
 
 gulp.task('default', ['serve']);
 
@@ -22,11 +16,8 @@ gulp.task('serve', ['less'], function() {
         server: "./"
     });
 
-    // gulp.watch("css/*.css", ['less']);
     gulp.watch("css/*.css").on('change', browserSync.reload);
     gulp.watch("index.html").on('change', browserSync.reload);
     gulp.watch("js/*.js").on('change', browserSync.reload);
-    // gulp.watch("js/**/*.js", ['js-watch']);
-
     gulp.watch('less/*.less', ['less']);
 });
