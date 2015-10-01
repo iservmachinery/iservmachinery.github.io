4a8335cafda3ecb27e4e6337ced36391707ac227
theHarvester <james.harvey994@gmail.com>
2015-10-01 22:15:38 +1000

    Added fb image

--- img/gallery/resize.sh
+++ img/gallery/resize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for file in *.jpg
 do
-    sips --resampleHeightWidthMax 512 --out out --setProperty formatOptions 85 $file
+    sips --resampleHeightWidthMax 1024 --out out --setProperty formatOptions 85 $file
 done;
